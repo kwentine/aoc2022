b50a098a8d768284b981e64c366a8c1eaa29aded
Quentin Agren <quentin.agren@backmarket.com>
2022-12-01 19:27:01 +0100

    Project skeleton

---
+++ template.py
@@ -0,0 +1,16 @@
+from utils import read_input
+
+DAY = 0
+
+def parse(input_str: str):
+    pass
+
+def part_one(data: str) -> int:
+    pass
+
+def part_two(data: str) -> int:
+    pass
+
+if __name__ == "__main__":
+    data = parse(read_input(day=DAY))
+    print(part_one(data))
---
+++ utils.py
@@ -0,0 +1,3 @@
+def read_input(day: int) -> str:
+    with open(f"data/day{day:02d}.txt") as f:
+        return f.read()
