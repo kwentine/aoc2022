010301e925c299d07ed3e1962c6ef0d638675931
Quentin Agren <quentin.agren@backmarket.com>
2022-12-02 13:30:28 +0100

    Day 02 part 2

--- day02.py
+++ day02.py
@@ -17,11 +17,16 @@ def part_one(data: str) -> int:
     return sum(score(*match_round) for match_round in data)
 
 def part_two(data: str) -> int:
-    pass
+    return sum(ad_hoc_score(*match_round) for match_round in data)
 
 def score(elf, mine):
     return (elf == (mine + 2) % 3) * 6 + (elf == mine) * 3 + mine + 1
 
+def ad_hoc_score(elf, outcome):
+    mine = (elf + (outcome - 1)) % 3
+    return 3 * outcome + mine + 1
+
 if __name__ == "__main__":
     data = parse(read_input(day=DAY))
     print(part_one(data))
+    print(part_two(data))
